ecf2183615341715accfa16fc45f5d747413f0f6
zhouli <545289437@qq.com>
2016-07-21 14:09:10 +0800

    modify

---
+++ src/main/java/com/zl/consumer/ServiceProxy.java
@@ -0,0 +1,59 @@
+package com.zl.consumer;
+
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.UUID;
+
+import com.zl.common.RpcRequest;
+import com.zl.common.RpcResponse;
+
+public class ServiceProxy {
+	private String serverAddress;
+    private ServiceDiscovery serviceDiscovery;
+
+    public ServiceProxy(String serverAddress) {
+        this.serverAddress = serverAddress;
+    }
+
+    public ServiceProxy(ServiceDiscovery serviceDiscovery) {
+        this.serviceDiscovery = serviceDiscovery;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T create(Class<?> interfaceClass) {
+        return (T) Proxy.newProxyInstance(
+            interfaceClass.getClassLoader(),
+            new Class<?>[]{interfaceClass},
+            new InvocationHandler() {
+            	
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    RpcRequest request = new RpcRequest(); // 创建并初始化 RPC 请求
+                    request.setRequestId(UUID.randomUUID().toString());
+                    request.setClassName(method.getDeclaringClass().getName());
+                    request.setMethodName(method.getName());
+                    request.setParameterTypes(method.getParameterTypes());
+                    request.setParameters(args);
+
+                    if (serviceDiscovery != null) {
+                        serverAddress = serviceDiscovery.selectOneServer(); // 发现服务
+                    }
+
+                    String[] array = serverAddress.split(":");
+                    String host = array[0];
+                    int port = Integer.parseInt(array[1]);
+
+                    ConsumerHandler client = new ConsumerHandler(host, port); // 初始化 RPC 客户端
+                    RpcResponse response = client.send(request); // 通过 RPC 客户端发送 RPC 请求并获取 RPC 响应
+
+                    if (response.isError()) {
+                        throw response.getError();
+                    } else {
+                        return response.getResult();
+                    }
+                }
+            }
+        );
+    }
+}
