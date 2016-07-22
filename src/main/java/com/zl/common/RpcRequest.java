1a08b606d3d027f42cd32f2b7925ad84eda85ff1
ZhouLi <545289437@qq.com>
2016-07-22 15:06:48 +0800

    commit

--- src/main/java/com/zl/common/RpcRequest.java
+++ src/main/java/com/zl/common/RpcRequest.java
@@ -8,9 +8,6 @@ import java.io.Serializable;
  */
 public class RpcRequest implements Serializable{
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	private String requestId;
