11808176dc7a7ea847a17433a029313b5439965f
ZhouLi <545289437@qq.com>
2016-08-19 15:09:28 +0800

    zl

--- src/main/java/com/zl/serialize/SerializationUtil.java
+++ src/main/java/com/zl/serialize/SerializationUtil.java
@@ -29,8 +29,6 @@ public class SerializationUtil {
 	// * Objenesis 不用反射创建
 	// */
 	// private static Map<Class<?>, Schema<?>> cachedSchema = new
-	
-	// T message = (T) objenesis.newInstance(cls); //用objenesis创建对象
-	
+	// T message = (T) objenesis.newInstance(cls); //用objenesis创建对象	
 	// }
 }
